4e298301fcfbd68563366c88f81160084aa65768
Pawawudaf <oleksandr.kukotin@gmail.com>
2023-02-11 17:03:35 +0200

    Initial commit

---
+++ src/main/java/com/example/jowl/JOwlApplication.java
@@ -0,0 +1,13 @@
+package com.example.jowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JOwlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JOwlApplication.class, args);
+    }
+
+}
