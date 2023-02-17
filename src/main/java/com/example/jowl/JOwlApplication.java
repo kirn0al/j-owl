d50439c5c6698220cdf4d75c78481c0979cd9b39
Pawawudaf <oleksandr.kukotin@gmail.com>
2023-02-18 01:03:08 +0200

    Created WebCrawler class and added new dependencies

--- src/main/java/com/example/jowl/JOwlApplication.java
+++ src/main/java/com/example/jowl/JOwlApplication.java
@@ -1,13 +1,14 @@
 package com.example.jowl;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JOwlApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JOwlApplication.class, args);
-    }
+    private final WebCrawler webCrawler = new WebCrawler();
 
+    public static void main(String[] args) {
+        JOwlApplication jOwl = new JOwlApplication();
+        jOwl.webCrawler.crawl("https://alibaba-cloud.medium.com/analysis-of-lucene-basic-concepts-5ff5d8b90a53");
+    }
 }
---
+++ src/main/java/com/example/jowl/WebCrawler.java
@@ -0,0 +1,71 @@
+package com.example.jowl;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class WebCrawler {
+
+    private final Set<String> visitedUrls;
+    private final Queue<String> urlsToVisit;
+
+    public WebCrawler() {
+        this.visitedUrls = new HashSet<>();
+        this.urlsToVisit = new LinkedList<>();
+    }
+    public void crawl(String seedUrl) {
+        urlsToVisit.add(seedUrl);
+        while (!urlsToVisit.isEmpty()) {
+            String url = urlsToVisit.remove();
+            visitedUrls.add(url);
+            String html = fetchHtml(url);
+            List<String> links = parseLinks(html);
+            for (String link : links) {
+                if (!visitedUrls.contains(link)) {
+                    urlsToVisit.add(link);
+                }
+            }
+            storeResults(html);
+        }
+    }
+
+    private String fetchHtml(String url) {
+        try {
+            Document html = Jsoup.connect(url).get();
+            return html.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    private List<String> parseLinks(String html) {
+        List<String> links = new ArrayList<>();
+        Document doc = Jsoup.parse(html);
+        Elements linkElements = doc.select("a[href]");
+
+        for (Element linkElement : linkElements) {
+            String link = linkElement.attr("href");
+            links.add(link);
+        }
+        return links;
+    }
+
+    private void storeResults(String html) {
+        try (PrintWriter out = new PrintWriter(new FileWriter("results.txt", true))) {
+            out.println(html);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
